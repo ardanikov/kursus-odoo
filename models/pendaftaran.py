14928665211b74bba0263da8bf23eadda28d26a9
ardanikov <sess.uuai@gmail.com>
2025-12-23 09:39:22 +0700

    remove unecessary comment

--- models/pendaftaran.py
+++ models/pendaftaran.py
@@ -10,7 +10,6 @@ class Pendaftaran(models.Model):
     jenis_kelamin = fields.Selection(string='Jenis Kelamin', related="pendaftar_id.jenis_kelamin", readonly=True)
     no_hp = fields.Char(string='No. HP', related='pendaftar_id.mobile', readonly=True)
     kursus_id = fields.Many2one(comodel_name='cdn.kursus', string='Kursus', required=True)
-    # session_id = fields.Many2one(comodel_name='cdn.kursus.session', string='Session', required=True)
     state = fields.Selection(string='Status', selection=[('draft', 'Draft'), ('confirm', 'Confirm')], default='draft')
     harga_kursus_total = fields.Float(string='Harga Kursus Total', related="kursus_id.harga_kursus_total", readonly=True)
     currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)
